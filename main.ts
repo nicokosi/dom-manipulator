44b9ee1c00468b35917d0516b8529f9ea4e05cb1
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-12-23 06:51:58 +0100

    Use checkboxes ☑️ + add style 💅 + reformat code 🧑‍✈️

--- main.ts
+++ main.ts
@@ -1,32 +1,29 @@
 const todoList = [
-    {title: 'Buy milk 🥛', prioriy: 'medium', notes: 'How much? Where?'},
-    {title: 'Do sport 🏃', prioriy: 'low', notes: ''},
-    {title: 'Sleep 😴', prioriy: 'high', notes: ''}
+    { title: 'Buy milk 🥛', prioriy: 'medium', notes: 'How much? Where?' },
+    { title: 'Do sport 🏃', prioriy: 'low', notes: '' },
+    { title: 'Sleep 😴', prioriy: 'high', notes: '' }
 ];
 
 document.getElementById('app').innerHTML = `
 <div class="todo-list">
-<ul>
 ${todoList
-    .map((item, index) => `
-    <li>
+        .map((item, index) => `
         <div
             id='item${index}'
             class='todo-list-item'
             onclick='document.querySelector("#detail${index}").style.display = "block"'>
-            ${item.title}
+            <input type="checkbox" id="todo-${index}">
+            <label for="todo-${index}">${item.title}</label>
         </div>
         <div
             id='detail${index}'
             class='todo-list-detail'
             onclick='document.querySelector("#detail${index}").style.display = "none"'>
             <a href="#" class="close"></a>
-            <div>Title: ${item.title}</div>
             <div>Priority: ${item.prioriy}</div>
             <div>Notes: ${item.notes}</div>
-        </div>
-    </li>`)
-    .join('')
-}
-</ul>
+        </div>`)
+        .join('')
+    }
+</input>
 </div>`
