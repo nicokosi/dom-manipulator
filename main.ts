23da58f6f2a7bbfc90fdf0f0598cf367a43e9370
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-01-13 07:11:29 +0100

    Display random names in task title via an HTTP service 🎲
    Increase end-to-end tests' timeout becayse the calls to
    external service add some latency. ⏳
    
    External service: https://random-words-api.vercel.app/word
    Code: https://github.com/mcnaveen/Random-Words-API

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 const verbs = ["Buy", "Sell", "Bring", "Clean", "Prepare", "Rent", "Find", "Grab", "Collect", "Borrow"]
-const nouns = ["something", "a car", "sport shoes", "smart ideas", "an ideal workplace", "coffee", "a mug", "skateboards", "some food", "crazy thinks"]
 const emojis = ["😇", "🚨", "😡", "♻️", "♬", "💅", "🎲", "☹️", "☺️", "😍", "⭐️", "🥶", "👏", "🔔", "🤡", "👉"]
 const priorities = ['low', 'medium', 'high']
 
-const randomTodoListGenerator = (size: number) =>
+async function randomWordFromExternalService() {
+    return fetch('https://random-words-api.vercel.app/word')
+        .then(response => response.json())
+        .then(data => data[0].word);
+}
+
+const randomTodoListGenerator = (size: number, nouns: Array<string>) =>
     Array.from({ length: size }, (_element, index) => {
         let randomTitle = verbs[Math.trunc(verbs.length * Math.random())]
             + " " +
@@ -19,54 +24,75 @@ const randomTodoListGenerator = (size: number) =>
         };
     });
 const numberOfItems = Math.trunc(2 + 4 * Math.random())
-let todoList = randomTodoListGenerator(numberOfItems)
+let randomWords = []
+let todoList
+
+var randomized = false;
 
 function render() {
-    document.getElementById('app').innerHTML = `
-    <div class="todo-list">
-    ${todoList
-            .filter((item) => item.status === 'TODO')
-            .map((item, index) => `
-            <div
-                id='item${index}'
-                class='todo-list-item'
-                onclick='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "block" }'>
-                <input type="checkbox" id="todo-${index}">
-                <label for="todo-${index}">${item.title}</label>
-            </div>
-            <div
-                id='detail${index}'
-                class='todo-list-detail'
-                onclick='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "none" }'>
-                <a href="#" class="close"></a>
-                <div>Priority: ${item.priority}</div>
-                <div>Notes: ${item.notes}</div>
-            </div>`)
-            .join('')
-        }
-    </input>
-    </div>`
+    Promise.all(
+        [
+            randomized ? Promise.resolve(randomWords[0]) : randomWordFromExternalService(),
+            randomized ? Promise.resolve(randomWords[1]) : randomWordFromExternalService(),
+            randomized ? Promise.resolve(randomWords[2]) : randomWordFromExternalService(),
+            randomized ? Promise.resolve(randomWords[3]) : randomWordFromExternalService(),
+            randomized ? Promise.resolve(randomWords[4]) : randomWordFromExternalService(),
+            randomized ? Promise.resolve(randomWords[5]) : randomWordFromExternalService()
+        ])
+        .then((words) => {
+            if (!randomized) {
+                randomWords = words
+                randomized = true
+                todoList = randomTodoListGenerator(numberOfItems, randomWords)
+            }
+            document.getElementById('app').innerHTML = `
+            <div class="todo-list">
+            ${todoList
+                    .filter((item) => item.status === 'TODO')
+                    .map((item, index) => `
+                    <div
+                        id='item${index}'
+                        class='todo-list-item'
+                        onclick='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "block" }'>
+                        <input type="checkbox" id="todo-${index}">
+                        <label for="todo-${index}">${item.title}</label>
+                    </div>
+                    <div
+                        id='detail${index}'
+                        class='todo-list-detail'
+                        onclick='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "none" }'>
+                        <a href="#" class="close"></a>
+                        <div>Priority: ${item.priority}</div>
+                        <div>Notes: ${item.notes}</div>
+                    </div>`)
+                    .join('')
+                }
+            </input>
+            </div>`
 
-    for (let index = 0; index < todoList.length; index++) {
-        document.getElementById(`todo-${index}`)
-            ?.addEventListener(
-                "click",
-                (event) => {
-                    todoList = todoList
-                        .map((item) => {
-                            return item.status === 'TODO' && item.id !== index ?
-                                item :
-                                {
-                                    id: item.id,
-                                    title: item.title,
-                                    priority: item.priority,
-                                    notes: item.notes,
-                                    status: 'DONE'
-                                }
+            for (let index = 0; index < todoList.length; index++) {
+                document.getElementById(`todo-${index}`)
+                    ?.addEventListener(
+                        "click",
+                        (event) => {
+                            todoList = todoList
+                                .map((item) => {
+                                    return item.status === 'TODO' && item.id !== index ?
+                                        item :
+                                        {
+                                            id: item.id,
+                                            title: item.title,
+                                            priority: item.priority,
+                                            notes: item.notes,
+                                            status: 'DONE'
+                                        }
+                                });
+                            render();
                         });
-                    render();
-                });
-    }
+            }
+
+        });
+
 }
 
 render();
