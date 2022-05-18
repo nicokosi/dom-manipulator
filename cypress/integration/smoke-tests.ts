538734defa1848b9fc29e4b335e6776faf9bd272
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-05-18 07:04:14 +0200

    Clean tests 🎨

--- cypress/integration/smoke-tests.ts
+++ cypress/integration/smoke-tests.ts
@@ -6,37 +6,37 @@ describe('Smoke tests', () => {
         method: 'GET',
         url: 'https://random-words-api.vercel.app/word',
       },
-      [{ "word": "fake-random-word" }])
+      [{ 'word': 'fake-random-word' }])
       .as('word')
   })
 
   it('Show the detail on hover 🔍', () => {
-    cy.visit('http://localhost:3000');
-    const todoIndex = 1;
+    cy.visit('http://localhost:3000')
+    const todoIndex = 1
     cy.get(`#item${todoIndex} > label`).trigger('mouseover')
 
     cy.get(`#detail${todoIndex}`)
-      .should('be.visible');
+      .should('be.visible')
 
-    cy.screenshot();
+    cy.screenshot()
   })
 
   it('Mark item as completed when clicking on checkbox 🙈', () => {
-    cy.visit('http://localhost:3000');
-    const todoIndex = 1;
+    cy.visit('http://localhost:3000')
+    const todoIndex = 1
     cy.get(`#item${todoIndex}`)
       .as('item1')
-      .should('not.have.class', 'completed');
+      .should('not.have.class', 'completed')
 
     cy.get(`#todo-${todoIndex}`)
-      .click();
+      .click()
 
     cy.get('@item1')
-      .should('have.class', 'completed');
+      .should('have.class', 'completed')
     })
 
     it('Check that DOM can be manipulated 😇', () => {
-      cy.visit('http://localhost:3000');
+      cy.visit('http://localhost:3000')
       document.body.remove()
       cy.get('body', { timeout: 10000 })
         .should('have.length', 1)
