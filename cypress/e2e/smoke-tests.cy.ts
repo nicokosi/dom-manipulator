7630b17e51b6560bac497b98a836ca75e1d394a7
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2022-09-18 18:31:54 +0200

    Install Prettier 🚨
    https://prettier.io/docs/en/install.html
    https://github.com/prettier/eslint-config-prettier#installation

--- cypress/e2e/smoke-tests.cy.ts
+++ cypress/e2e/smoke-tests.cy.ts
@@ -1,62 +1,57 @@
-describe('Smoke tests', () => {
-  function mockRandomWordsService () {
+describe("Smoke tests", () => {
+  function mockRandomWordsService() {
     cy.intercept(
       {
-        method: 'GET',
-        url: 'https://random-words-api.vercel.app/word'
+        method: "GET",
+        url: "https://random-words-api.vercel.app/word",
       },
-      [{ word: 'fake-random-word' }])
-      .as('word')
+      [{ word: "fake-random-word" }]
+    ).as("word");
   }
 
-  it('Show loading message ⏳', () => {
-    let loadWords
-    const trigger = new Cypress.Promise((resolve) => (loadWords = resolve))
-    cy.intercept('https://random-words-api.vercel.app/word', (req) => {
-      return trigger.then(() => req.reply([{ word: 'fake-random-word' }]))
-    })
+  it("Show loading message ⏳", () => {
+    let loadWords;
+    const trigger = new Cypress.Promise((resolve) => (loadWords = resolve));
+    cy.intercept("https://random-words-api.vercel.app/word", (req) => {
+      return trigger.then(() => req.reply([{ word: "fake-random-word" }]));
+    });
 
-    cy.visit('http://localhost:3000')
+    cy.visit("http://localhost:3000");
 
-    cy.get('span.message')
-      .should('have.text', 'Loading tasks…')
-      .then(loadWords)
-    cy.get('span.message')
-      .should('have.text', '')
-  })
+    cy.get("span.message")
+      .should("have.text", "Loading tasks…")
+      .then(loadWords);
+    cy.get("span.message").should("have.text", "");
+  });
 
-  it('Show the detail on hover 🔍', () => {
-    mockRandomWordsService()
-    cy.visit('http://localhost:3000')
+  it("Show the detail on hover 🔍", () => {
+    mockRandomWordsService();
+    cy.visit("http://localhost:3000");
 
-    const todoIndex = 1
-    cy.get(`#item${todoIndex} > label`).trigger('mouseover')
+    const todoIndex = 1;
+    cy.get(`#item${todoIndex} > label`).trigger("mouseover");
 
-    cy.get(`#detail${todoIndex}`)
-      .should('be.visible')
-    cy.screenshot()
-  })
+    cy.get(`#detail${todoIndex}`).should("be.visible");
+    cy.screenshot();
+  });
 
-  it('Mark item as completed when clicking on checkbox 🙈', () => {
-    mockRandomWordsService()
-    cy.visit('http://localhost:3000')
-    const todoIndex = 1
+  it("Mark item as completed when clicking on checkbox 🙈", () => {
+    mockRandomWordsService();
+    cy.visit("http://localhost:3000");
+    const todoIndex = 1;
     cy.get(`#item${todoIndex}`)
-      .as('item1')
-      .should('not.have.class', 'completed')
+      .as("item1")
+      .should("not.have.class", "completed");
 
-    cy.get(`#todo-${todoIndex}`)
-      .click()
+    cy.get(`#todo-${todoIndex}`).click();
 
-    cy.get('@item1')
-      .should('have.class', 'completed')
-  })
+    cy.get("@item1").should("have.class", "completed");
+  });
 
-  it('Check that DOM can be manipulated 😇', () => {
-    mockRandomWordsService()
-    cy.visit('http://localhost:3000')
-    document.body.remove()
-    cy.get('body', { timeout: 10000 })
-      .should('have.length', 1)
-  })
-})
+  it("Check that DOM can be manipulated 😇", () => {
+    mockRandomWordsService();
+    cy.visit("http://localhost:3000");
+    document.body.remove();
+    cy.get("body", { timeout: 10000 }).should("have.length", 1);
+  });
+});
--- main.ts
+++ main.ts
@@ -1,61 +1,105 @@
-const verbs = ['Buy', 'Sell', 'Bring', 'Clean', 'Prepare', 'Rent', 'Find', 'Grab', 'Collect', 'Borrow']
-const emojis = ['😇', '🚨', '😡', '♻️', '♬', '💅', '🎲', '☹️', '☺️', '😍', '⭐️', '🥶', '👏', '🔔', '🤡', '👉']
-const priorities = ['low', 'medium', 'high']
+const verbs = [
+  "Buy",
+  "Sell",
+  "Bring",
+  "Clean",
+  "Prepare",
+  "Rent",
+  "Find",
+  "Grab",
+  "Collect",
+  "Borrow",
+];
+const emojis = [
+  "😇",
+  "🚨",
+  "😡",
+  "♻️",
+  "♬",
+  "💅",
+  "🎲",
+  "☹️",
+  "☺️",
+  "😍",
+  "⭐️",
+  "🥶",
+  "👏",
+  "🔔",
+  "🤡",
+  "👉",
+];
+const priorities = ["low", "medium", "high"];
 
-async function randomWordFromExternalService () {
-  return fetch('https://random-words-api.vercel.app/word')
-    .then(response => response.json())
-    .then(data => data[0].word)
+async function randomWordFromExternalService() {
+  return fetch("https://random-words-api.vercel.app/word")
+    .then((response) => response.json())
+    .then((data) => data[0].word);
 }
 
 const randomTodoListGenerator = (size: number, nouns: Array<string>) =>
   Array.from({ length: size }, (_element, index) => {
-    const randomTitle = verbs[Math.trunc(verbs.length * Math.random())] +
-            ' ' +
-            nouns[Math.trunc(nouns.length * Math.random())] +
-            ' ' +
-            emojis[Math.trunc(emojis.length * Math.random())]
+    const randomTitle =
+      verbs[Math.trunc(verbs.length * Math.random())] +
+      " " +
+      nouns[Math.trunc(nouns.length * Math.random())] +
+      " " +
+      emojis[Math.trunc(emojis.length * Math.random())];
     return {
       id: index,
       title: randomTitle,
       priority: priorities[Math.trunc(3 * Math.random())],
-      notes: '',
-      status: 'TODO'
+      notes: "",
+      status: "TODO",
+    };
+  });
+const numberOfItems = Math.trunc(2 + 4 * Math.random());
+let randomWords = [];
+let todoList;
+
+let randomized = false;
+
+function render() {
+  Promise.all([
+    randomized
+      ? Promise.resolve(randomWords[0])
+      : randomWordFromExternalService(),
+    randomized
+      ? Promise.resolve(randomWords[1])
+      : randomWordFromExternalService(),
+    randomized
+      ? Promise.resolve(randomWords[2])
+      : randomWordFromExternalService(),
+    randomized
+      ? Promise.resolve(randomWords[3])
+      : randomWordFromExternalService(),
+    randomized
+      ? Promise.resolve(randomWords[4])
+      : randomWordFromExternalService(),
+    randomized
+      ? Promise.resolve(randomWords[5])
+      : randomWordFromExternalService(),
+  ]).then((words) => {
+    if (!randomized) {
+      randomWords = words;
+      randomized = true;
+      todoList = randomTodoListGenerator(numberOfItems, randomWords);
     }
-  })
-const numberOfItems = Math.trunc(2 + 4 * Math.random())
-let randomWords = []
-let todoList
-
-let randomized = false
-
-function render () {
-  Promise.all(
-    [
-      randomized ? Promise.resolve(randomWords[0]) : randomWordFromExternalService(),
-      randomized ? Promise.resolve(randomWords[1]) : randomWordFromExternalService(),
-      randomized ? Promise.resolve(randomWords[2]) : randomWordFromExternalService(),
-      randomized ? Promise.resolve(randomWords[3]) : randomWordFromExternalService(),
-      randomized ? Promise.resolve(randomWords[4]) : randomWordFromExternalService(),
-      randomized ? Promise.resolve(randomWords[5]) : randomWordFromExternalService()
-    ])
-    .then((words) => {
-      if (!randomized) {
-        randomWords = words
-        randomized = true
-        todoList = randomTodoListGenerator(numberOfItems, randomWords)
-      }
-      document.getElementById('app').innerHTML = `
+    document.getElementById("app").innerHTML = `
             <div class="todo-list">
             <ul>
             ${todoList
-                    .map((item, index) => `
+              .map(
+                (item, index) => `
                     <li
                         id='item${index}'
-                        class='todo-list-item ${item.status === 'DONE' ? 'completed' : ''}'
+                        class='todo-list-item ${
+                          item.status === "DONE" ? "completed" : ""
+                        }'
                         onmouseover='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "block" }'
                         onmouseout='if (document.querySelector("#detail${index}")) { document.querySelector("#detail${index}").style.display = "none" }'>
-                        <input type="checkbox" id="todo-${index}" ${item.status === 'DONE' ? 'checked' : ''}>
+                        <input type="checkbox" id="todo-${index}" ${
+                  item.status === "DONE" ? "checked" : ""
+                }>
                         <label for="todo-${index}">${item.title}</label>
                     </li>
                     <div
@@ -64,34 +108,32 @@ function render () {
                         <a href="#" class="close"></a>
                         <div>Priority: ${item.priority}</div>
                         <div>Notes: ${item.notes}</div>
-                    </div>`)
-                    .join('')
-                }
+                    </div>`
+              )
+              .join("")}
             </ul>
-            </div>`
+            </div>`;
 
-      for (let index = 0; index < todoList.length; index++) {
-        document.getElementById(`todo-${index}`)
-          ?.addEventListener(
-            'input',
-            (event) => {
-              todoList = todoList
-                .map((item) => {
-                  return item.status === 'TODO' && item.id !== index
-                    ? item
-                    : {
-                        id: item.id,
-                        title: item.title,
-                        priority: item.priority,
-                        notes: item.notes,
-                        status: 'DONE'
-                      }
-                })
-              render()
-            })
-        document.querySelector('span.message').innerHTML = ''
-      }
-    })
+    for (let index = 0; index < todoList.length; index++) {
+      document
+        .getElementById(`todo-${index}`)
+        ?.addEventListener("input", (event) => {
+          todoList = todoList.map((item) => {
+            return item.status === "TODO" && item.id !== index
+              ? item
+              : {
+                  id: item.id,
+                  title: item.title,
+                  priority: item.priority,
+                  notes: item.notes,
+                  status: "DONE",
+                };
+          });
+          render();
+        });
+      document.querySelector("span.message").innerHTML = "";
+    }
+  });
 }
 
-render()
+render();
